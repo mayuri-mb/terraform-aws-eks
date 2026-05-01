6f72331fef75a52a3a6fe07cf3446a6cfeab3cf8
mayuri <mayu.reddy10@gmail.com>
2026-05-01 10:15:03 +1000

    terraform

--- 30-rds/locals.tf
+++ 30-rds/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.name   
+    database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value 
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     common_tags = {
         project = var.project
